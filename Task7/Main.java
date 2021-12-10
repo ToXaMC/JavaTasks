e7ec78f78da073cb4f1f210dae2d069582ab3592
ToXaMC <maki.net@yandex.ru>
2021-12-10 17:17:05 +0300

    1.1

---
+++ Task7/Main.java
@@ -0,0 +1,9 @@
+package Task7;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Harakteristic sob = new Harakteristic("big","Black","Mobs","Bob");
+        sob.getInfo();
+    }
+}
